55815c3c99f3379511bf10dd3d59b1ede39bdb74
wangqicc <1720905626@qq.com>
2021-03-27 23:34:48 +0800

    添加-java 工具包初始化

---
+++ src/main/java/Utils/FileUtil.java
@@ -0,0 +1,43 @@
+package Utils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+
+    /**
+     * 文件读取类
+     * @param filepath        文件路径
+     * @param encoding        编码方式
+     * @return                文件列表
+     * @throws IOException    文件读取异常
+     */
+    public static List<String> readLines(String filepath, String encoding) throws IOException {
+        List<String> lines = new ArrayList<>();
+        File file = new File(filepath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+        while (reader.ready()) {
+            lines.add(reader.readLine());
+        }
+        reader.close();
+        return lines;
+    }
+
+    /**
+     * 文件写入
+     * @param filepath        文件路径
+     * @param encoding        编码方式
+     * @param lines           文件列表
+     * @throws IOException    文件写入异常
+     */
+    public static void writeLines(String filepath, String encoding, List<String> lines) throws IOException {
+        File file = new File(filepath);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+        for (String line : lines) {
+            writer.write(line+"\n");
+        }
+        writer.close();
+    }
+
+}
---
+++ src/test/java/TestFileUtil.java
@@ -0,0 +1,23 @@
+import Utils.FileUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFileUtil {
+
+    public static void main(String[] args) throws IOException {
+        String filename = "src/test/data/test.txt";
+        List<String> lines = new ArrayList<>();
+        lines.add("wq");
+        lines.add("xsl");
+
+        // 文件写入
+        FileUtil.writeLines(filename, "UTF-8", lines);
+
+        // 文件读取
+        List<String> list = FileUtil.readLines(filename, "UTF-8");
+        System.out.println(list);
+    }
+
+}
