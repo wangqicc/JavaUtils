d167b7ee4821dd3c5623f76d019507d57fb0cbcb
wangqicc <1720905626@qq.com>
2021-05-05 16:37:21 +0800

    【更新】List 排序（使用 Comparator）

--- src/test/java/TestSortAlgorithm.java
+++ src/test/java/TestSortAlgorithm.java
@@ -1,12 +1,23 @@
 import algorithm.SortAlgorithm;
+import entity.Person;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+
+/**
+ * 按照年龄对 Person 进行排序
+ */
+class AgeCompare implements Comparator<Person> {
+    public int compare(Person o1, Person o2) {
+        return o1.getAge() - o2.getAge();
+    }
+}
+
 
 public class TestSortAlgorithm {
 
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<Integer>();
+        List<Integer> arr = new ArrayList<>();
         arr.add(1);
         arr.add(3);
         arr.add(1);
@@ -27,6 +38,15 @@ public class TestSortAlgorithm {
 
         SortAlgorithm.selectSort(arr);
         System.out.println("select sort: " + arr);
+
+        List<Person> ps = new ArrayList<>();
+        ps.add(new Person("jack", 24));
+        ps.add(new Person("milk", 10));
+        // JDK8 中可以使用 Comparator 表达式进行构建迭代器
+        // 不过还是推荐下面的一种写法，先利用 Comparator 构建好排序规则，然后再新建这个规则的对象的方式进行排序
+//        ps.sort(Comparator.comparingInt(Person::getAge));
+        ps.sort(new AgeCompare());
+        System.out.println(ps);
     }
 
 }
